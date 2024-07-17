e15eb125d184ff8e6fa9cffe46ccd1be80c74c91
jurchiks33 <jurisslisans@gmail.com>
2024-07-17 21:22:45 +0200

    scatterplot

--- sandbox.py
+++ sandbox.py
@@ -21,3 +21,30 @@ print('explain variance ratio (first two components) : %s' %
       str(pca.explained_variance_ratio_))
 print('explain variance ratio (first two components) : %s' %
       str(sum(pca.explained_variance_ratio_)))
+
+
+
+x = np.arange(10)
+ys = [i+x+(i*x)**2 for i in range(10)]
+
+plt.figure()
+colors = cm.rainbow(np.linspace(0, 1, len(ys)))
+for c, i, target_name in zip(colors, [1,2,3,4,5,6,7,8,9,10], labels):
+    plt.scatter(data_r[labels == i, 0], data_r[labels == i, 1],
+    c=c, alpha= 0.4)
+    plt.legend()
+    plt.title('Scatterplot of Points plotted in first \n'
+              '10 Principal Components')
+    plt.show()
+
+# plt.figure()
+# colors = cm.rainbow(np.linspace(0, 1, len(np.unique(labels))))
+# target_names = ['Class 1', 'Class 2', 'Class 3', 'Class 4', 'Class 5',
+#                 'Class 6', 'Class 7', 'Class 8', 'Class 9', 'Class 10']
+
+# for c, i, target_name in zip(colors, range(1, 11), target_names):
+#     plt.scatter(data_r[labels == i, 0], data_r[labels == i, 1], c=[c], alpha=0.4, label=target_name)
+
+# plt.legend()
+# plt.title('Scatterplot of Points plotted in first \n10 Principal Components')
+# plt.show()
