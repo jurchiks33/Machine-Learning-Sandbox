ebc767efefd25483f6453d6a6a4a91ba189bd0d3
jurchiks33 <jurisslisans@gmail.com>
2024-07-17 21:42:22 +0200

    klustering

--- sandbox.py
+++ sandbox.py
@@ -14,8 +14,8 @@ n_digits = len(np.unique(digits.target))
 labels = digits.target
 
 
-# pca = PCA(n_components=10)
-# data_r = pca.fit(data).transform(data)
+pca = PCA(n_components=10)
+data_r = pca.fit(data).transform(data)
 
 # print('explain variance ratio (first two components) : %s' %
 #       str(pca.explained_variance_ratio_))
