9c2973b9b888506da23ade14531b6627258dd157
jurchiks33 <jurisslisans@gmail.com>
2024-07-17 21:04:55 +0200

    initialize PCA algorithm

--- sandbox.py
+++ sandbox.py
@@ -12,3 +12,12 @@ data = digits.data
 n_samples, n_features = data.shape
 n_digits = len(np.unique(digits.target))
 labels = digits.target
+
+
+pca = PCA(n_components=10)
+data_r = pca.fit(data).transform(data)
+
+print('explain variance ratio (first two components) : %s' %
+      str(pca.explained_variance_ratio_))
+print('explain variance ratio (first two components) : %s' %
+      str(sum(pca.explained_variance_ratio_)))
