d741a707f903f609178e94c46227a49f3d7711ba
jurchiks33 <jurisslisans@gmail.com>
2024-07-17 21:26:29 +0200

    clustering analysis

--- sandbox.py
+++ sandbox.py
@@ -1,41 +1,41 @@
-import numpy as np
-from sklearn.datasets import load_digits
-import matplotlib.pyplot as plt
-from sklearn.decomposition import PCA 
-from sklearn.preprocessing import scale
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
-import matplotlib.cm as cm
+# import numpy as np
+# from sklearn.datasets import load_digits
+# import matplotlib.pyplot as plt
+# from sklearn.decomposition import PCA 
+# from sklearn.preprocessing import scale
+# from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
+# import matplotlib.cm as cm
 
-digits = load_digits()
-data = digits.data
+# digits = load_digits()
+# data = digits.data
 
-n_samples, n_features = data.shape
-n_digits = len(np.unique(digits.target))
-labels = digits.target
-
-
-pca = PCA(n_components=10)
-data_r = pca.fit(data).transform(data)
-
-print('explain variance ratio (first two components) : %s' %
-      str(pca.explained_variance_ratio_))
-print('explain variance ratio (first two components) : %s' %
-      str(sum(pca.explained_variance_ratio_)))
+# n_samples, n_features = data.shape
+# n_digits = len(np.unique(digits.target))
+# labels = digits.target
 
 
+# pca = PCA(n_components=10)
+# data_r = pca.fit(data).transform(data)
 
-x = np.arange(10)
-ys = [i+x+(i*x)**2 for i in range(10)]
+# print('explain variance ratio (first two components) : %s' %
+#       str(pca.explained_variance_ratio_))
+# print('explain variance ratio (first two components) : %s' %
+#       str(sum(pca.explained_variance_ratio_)))
 
-plt.figure()
-colors = cm.rainbow(np.linspace(0, 1, len(ys)))
-for c, i, target_name in zip(colors, [1,2,3,4,5,6,7,8,9,10], labels):
-    plt.scatter(data_r[labels == i, 0], data_r[labels == i, 1],
-    c=c, alpha= 0.4)
-    plt.legend()
-    plt.title('Scatterplot of Points plotted in first \n'
-              '10 Principal Components')
-    plt.show()
+
+
+# x = np.arange(10)
+# ys = [i+x+(i*x)**2 for i in range(10)]
+
+# plt.figure()
+# colors = cm.rainbow(np.linspace(0, 1, len(ys)))
+# for c, i, target_name in zip(colors, [1,2,3,4,5,6,7,8,9,10], labels):
+#     plt.scatter(data_r[labels == i, 0], data_r[labels == i, 1],
+#     c=c, alpha= 0.4)
+#     plt.legend()
+#     plt.title('Scatterplot of Points plotted in first \n'
+#               '10 Principal Components')
+#     plt.show()
 
 # plt.figure()
 # colors = cm.rainbow(np.linspace(0, 1, len(np.unique(labels))))
@@ -48,3 +48,11 @@ for c, i, target_name in zip(colors, [1,2,3,4,5,6,7,8,9,10], labels):
 # plt.legend()
 # plt.title('Scatterplot of Points plotted in first \n10 Principal Components')
 # plt.show()
+
+
+from time import time
+import numpy as np
+import matplotlib.pyplot as plt
+
+np.random.seed()
+
